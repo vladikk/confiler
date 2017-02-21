7a3c8f64eccca6a6d5313d5285c3603ba93c0ea7
Vladik Khononov <vladik@khononov.com>
2017-02-21 23:31:03 +0200

    initital steps file"

---
+++ steps/conf-merging.py
@@ -0,0 +1,55 @@
+@given(u'"threshold" is set to "10" for the dev environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given "threshold" is set to "10" for the dev environment')
+
+@when(u'configuration data is compiled for the dev environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When configuration data is compiled for the dev environment')
+
+@then(u'"threshold" is "10"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then "threshold" is "10"')
+
+@given(u'"threshold" is set to "5" for the dev.vladik environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given "threshold" is set to "5" for the dev.vladik environment')
+
+@when(u'configuration data is compiled for the dev.vladik environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When configuration data is compiled for the dev.vladik environment')
+
+@then(u'"threshold" is "5"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then "threshold" is "5"')
+
+@then(u'an error is raised')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then an error is raised')
+
+@given(u'"threshold" is set to "5" for the dev environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given "threshold" is set to "5" for the dev environment')
+
+@given(u'"hosts" is set to the following values for the dev environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given "hosts" is set to the following values for the dev environment')
+
+@then(u'"hosts" holds the following values')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then "hosts" holds the following values')
+
+@given(u'"hosts" is set to the following values for the dev.vladik environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given "hosts" is set to the following values for the dev.vladik environment')
+
+@given(u'the following items are set to be deleted from "hosts" in the dev.vladik environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the following items are set to be deleted from "hosts" in the dev.vladik environment')
+
+@given(u'the following items are added to the "hosts" collection in the dev.vladik environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the following items are added to the "hosts" collection in the dev.vladik environment')
+
+@given(u'the following items are set to be updated in the "hosts" collection in the dev.vladik environment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the following items are set to be updated in the "hosts" collection in the dev.vladik environment')
