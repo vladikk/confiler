9bab293ee4a7fc3c67b7bc0217888ecb4d797b1d
Vladik Khononov <vladik@khononov.com>
2017-02-22 17:19:20 +0200

    loading json files

--- confiler/api.py
+++ confiler/api.py
@@ -1,2 +1,22 @@
-def compile():
-  pass
+import os
+import json
+
+namespace_delimeter = "."
+env_file_extension = ".env"
+
+def compile(environments_folder, target_env):
+  parts = target_env.split(namespace_delimeter)
+  indexes = range(1, len(parts) + 1)
+  indexes.reverse()
+  for i in indexes:
+    namespace = namespace_delimeter.join(parts[0, i])
+    env = load_env(environments_folder, env_name)
+
+def load_env(environments_folder, env_name):
+  file_name = '%s.%s' % (env_name, env_file_extension)
+  file_path = os.path.join(environments_folder, file_name)
+  if not os.path.exists(file_path):
+    return { }
+  with (file_path, "w") as json_file:
+    env_json = json_file.read()
+    return json.loads(env_json)
