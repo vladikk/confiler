e811e7edb4915724242cbf9792c38377cae0cba8
Vladik Khononov <vladik@khononov.com>
2017-02-22 15:56:46 +0200

    ugly solution for importing confiler module in tests

--- tests/steps/compiling.py
+++ tests/steps/compiling.py
@@ -1,5 +1,11 @@
 import os
 from behave import *
+import json
+import sys
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../../')
+
+import confiler
 
 @given(u'the following config file is set for the {env_name} environment')
 def step_impl(context, env_name):
@@ -9,8 +15,18 @@ def step_impl(context, env_name):
 
 @when(u'configuration data is compiled for the {env_name} environment')
 def step_impl(context, env_name):
-  pass
+  context.result_json = confiler.compile(context.work_dir, env_name)
 
 @then(u'the result is the following configuration document')
 def step_impl(context):
-  pass
+  result = json.loads(context.result_json)
+  expected = json.loads(context.text)
+  assert ordered(result) == ordered(expected)
+
+def ordered(obj):
+  if isinstance(obj, dict):
+    return sorted((k, ordered(v)) for k, v in obj.items())
+  if isinstance(obj, list):
+    return sorted(ordered(x) for x in obj)
+  else:
+    return obj
