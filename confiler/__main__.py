f6eff06867f2e06e81b26e092be7d073b4b4e706
Vladik Khononov <vladik@khononov.com>
2017-02-26 16:01:28 +0200

    args parsing

--- confiler/__main__.py
+++ confiler/__main__.py
@@ -1,13 +1,29 @@
 import sys
-
+import argparse
+import confiler
 
 def main(args=None):
-    """The main routine."""
-    if args is None:
-        args = sys.argv[1:]
+  """The main routine."""
+  if args is None:
+    args = sys.argv[1:]
 
-    print("This is the main routine.")
-    print("It should do something interesting.")
+  parser = argparse.ArgumentParser(
+    description='Confiler: Configuration files compiler',
+    epilog="Example: confiler /myapp/envs dev.vladik /myapp/packages/dev.vladik --templates /myapp/src")
+  parser.add_argument("environments", help="Folder containing environments' configurations")
+  parser.add_argument("environment", help="Name of the targetted environment")
+  parser.add_argument("destination", help="Destination folder")
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument("-t", "--template", help="Template file that should be rendered")
+  group.add_argument("-s", "--templates", help="Templates folder that should be rendered")
+  args = parser.parse_args()
+  if not (args.template or args.templates):
+    parser.error('No template was set. Please add -t/--template or -s/--templates')
+  confiler.render(environment_name=args.environment,
+                  environments_path=args.environments,
+                  target_path=args.destination,
+                  templates_path=args.templates,
+                  template_file_path=args.template)
 
 if __name__ == "__main__":
     main()
