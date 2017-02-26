682d0d3f37d8da58ae7367701ae723d7634d748e
Vladik Khononov <vladik@khononov.com>
2017-02-26 16:32:42 +0200

    requirements management

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='confiler',
       version='0.1.0',
       author='Vladik Khononov',
       packages=['confiler'],
+      install_requires=reqs,
       entry_points={
           'console_scripts': [
               'confiler = confiler.__main__:main'
